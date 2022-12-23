030e3958ade1e8773c454e7269acf539159d92b2
황홍필 <marigold410@naver.com>
2022-12-23 20:41:55 +0900

    처음으로 만드는 커밋!

--- git_check/ViewController.swift
+++ git_check/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var name: String?
+    
+    
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    
+    
+    
+    
 
 }
 
